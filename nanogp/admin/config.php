9a8bf18dddf9efad7df72a4ffcf1e781c31caf3e
Francis Garrity <Isabeljmack94@gmail.com>
2022-03-10 17:47:26 +0800

    test

--- nanogp/admin/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$cfg_client_id          = 'your Google API client id';
-$cfg_client_secret      = 'your Google API client secret';
-$cfg_max_accounts       = 1;
-$cfg_log                = false;
-$albums_filter          = ['sauvegarde', 'backup'];
-?>
